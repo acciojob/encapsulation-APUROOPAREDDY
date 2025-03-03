f1ad6e6e3f7330b38cb3f0dcc5e63b7b7b9b30fb
APUROOPAREDDY <91656853+APUROOPAREDDY@users.noreply.github.com>
2025-03-03 15:54:31 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,7 +3,7 @@ package com.driver;
 public class Main {
   public static void main(String[] args){
     RWOnly obj=new RWOnly();
-    obj.name="Apuroopa";
-    System.out.println(obj.name);
+    obj.setName("Apuroopa");
+    System.out.println(obj.getName());
   }
 }
